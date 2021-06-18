3261c6842ccd45c432a9a43701e60f3994aabc1d
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-06-18 10:56:49 -0500

    Directiva *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Person } from './person.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularListPersonApp';
+  persons: Person[] = [
+    new Person('Álvaro', 'Barrera'),
+    new Person('Juan', 'Perez'),
+  ];
 }
