ee8de6e3a0b0d31b51a2333a7564fbc827451f3a
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-06-18 12:32:09 -0500

    ViewChild

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Person } from '../person.model';
 
 @Component({
@@ -10,9 +16,15 @@ export class FormComponent {
   @Output() personCreate = new EventEmitter<Person>();
   // nameInput: string = '';
   // lastnameInput: string = '';
-  storePerson(nameRef: HTMLInputElement, lastnameRef: HTMLInputElement) {
+  @ViewChild('nameRef') nameRef: ElementRef;
+  @ViewChild('lastnameRef') lastnameRef: ElementRef;
+
+  storePerson() {
     // let person = new Person(this.nameInput, this.lastnameInput);
-    let person = new Person(nameRef.value, lastnameRef.value);
+    let person = new Person(
+      this.nameRef.nativeElement.value,
+      this.lastnameRef.nativeElement.value
+    );
     this.personCreate.emit(person);
   }
 }
