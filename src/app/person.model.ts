8e6b759f77ef002b2d5bb8e44bd50249b217e93a
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-06-18 10:52:41 -0500

    Clase persona

---
+++ src/app/person.model.ts
@@ -0,0 +1,9 @@
+export class Person {
+  // name: string = '';
+  // lastname: string = '';
+  // constructor(name:string,lastname: string){
+  //   this.name = name;
+  //   this.lastname = lastname;
+  // }
+  constructor(public name: string, public lastname: string) {}
+}
