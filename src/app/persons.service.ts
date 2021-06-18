86655f2ebc5bb4b710fa58eb65f96cf91aefd054
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-06-18 12:59:45 -0500

    Decorador Injectable, inyectando LoggingService en PersonsService

--- src/app/persons.service.ts
+++ src/app/persons.service.ts
@@ -1,15 +1,20 @@
+import { Injectable } from '@angular/core';
+import { LoggingService } from './LoggingService.service';
 import { Person } from './person.model';
 
+@Injectable()
 export class PersonsService {
   persons: Person[] = [
     new Person('Álvaro', 'Barrera'),
     new Person('Juan', 'Perez'),
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   addPerson(person: Person) {
     this.persons.push(person);
-    // this.logginService.sendMessageToConsole(
-    //   `Persona agregada al arreglo: ${person.name}`
-    // );
+    this.loggingService.sendMessageToConsole(
+      `Persona agregada al arreglo: ${person.name}`
+    );
   }
 }
