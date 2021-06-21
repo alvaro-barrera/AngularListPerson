a197ce3b1f0826c7e7968a03ba113da45be45733
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-06-21 18:39:57 -0500

    RouterModule y router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './persons/form/form.component';
+import { PersonsComponent } from './persons/persons.component';
 
+
+const routes: Routes = [
+  {path: "", component: PersonsComponent},
+  {path: "persons/create", component: FormComponent},
+  {path: "persons/:id", component: FormComponent}
+]
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(
+      routes
+    )
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
