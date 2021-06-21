1bb35a856e35bb441f859e8f60daffe4fc574c44
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-06-21 18:30:17 -0500

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PersonComponent } from './person/person.component';
@@ -10,7 +11,7 @@ import { PersonsService } from './persons.service';
 
 @NgModule({
   declarations: [AppComponent, PersonComponent, FormComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [LoggingService, PersonsService],
   bootstrap: [AppComponent],
 })
